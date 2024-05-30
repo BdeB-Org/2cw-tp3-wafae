fcfb0ec94752ad311126d072d3039971900a611d
WafaeElkita <6263586@bdeb.qc.ca>
2024-05-30 19:09:40 -0400

    ajout code javascript pour page categorie2

---
+++ javascript/categorie2.js
@@ -0,0 +1,12 @@
+ document.getElementById('produit3Link').addEventListener('click', function() {
+            document.getElementById('histoireImage').style.display = 'none';
+            document.getElementById('produit3').style.display = 'block';
+            document.getElementById('produit4').style.display = 'none';
+        });
+
+       
+        document.getElementById('produit4Link').addEventListener('click', function() {
+            document.getElementById('histoireImage').style.display = 'none';
+            document.getElementById('produit3').style.display = 'none';
+            document.getElementById('produit4').style.display = 'block';
+        });
