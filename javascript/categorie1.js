c07a4088c0aa05ef97804fd5f1ba26837a517ea0
WafaeElkita <6263586@bdeb.qc.ca>
2024-05-30 17:59:00 -0400

    ajout les produit sur page categorie1

---
+++ javascript/categorie1.js
@@ -0,0 +1,13 @@
+ 
+        document.getElementById('produit1Link').addEventListener('click', function() {
+            document.getElementById('histoireImage').style.display = 'none';
+            document.getElementById('produit1').style.display = 'block';
+            document.getElementById('produit2').style.display = 'none';
+        });
+
+       
+        document.getElementById('produit2Link').addEventListener('click', function() {
+            document.getElementById('histoireImage').style.display = 'none';
+            document.getElementById('produit1').style.display = 'none';
+            document.getElementById('produit2').style.display = 'block';
+        });
