6dea1ae5f439689ca0ee16507eb1cab3d60b7a94
naomie8 <2266350@bdeb.qc.ca>
2024-05-31 21:41:35 -0400

    ajout boutton dans catégorie

--- javascript/categorie1.js
+++ javascript/categorie1.js
@@ -11,3 +11,18 @@
             document.getElementById('produit1').style.display = 'none';
             document.getElementById('produit2').style.display = 'block';
         });
+
+function afficherHistoire(){
+    const histoire = document.getElementById('histoire');
+    const button = document.getElementById('toggleButton');
+    if (histoire.style.display === "none") {
+        histoire.style.display = "block";
+        button.textContent = "Cacher l'histoire";
+    } else {
+        histoire.style.display = "none";
+        button.textContent = "Afficher l'histoire";
+    }
+
+}
+
+    
--- javascript/categorie2.js
+++ javascript/categorie2.js
@@ -10,3 +10,16 @@
             document.getElementById('produit3').style.display = 'none';
             document.getElementById('produit4').style.display = 'block';
         });
+
+        function afficherHistoire(){
+            const histoire = document.getElementById('histoire');
+            const button = document.getElementById('toggleButton');
+            if (histoire.style.display === "none") {
+                histoire.style.display = "block";
+                button.textContent = "Cacher l'histoire";
+            } else {
+                histoire.style.display = "none";
+                button.textContent = "Afficher l'histoire";
+            }
+        
+        }
