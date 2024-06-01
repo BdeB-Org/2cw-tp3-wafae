5468e0a0977b43a58cface70564a36370c857eab
naomie8 <2266350@bdeb.qc.ca>
2024-05-31 21:56:28 -0400

    ajustement des bouttons

--- javascript/categorie1.js
+++ javascript/categorie1.js
@@ -1,17 +1,3 @@
- 
-        document.getElementById('produit1Link').addEventListener('click', function() {
-            document.getElementById('histoireImage').style.display = 'none';
-            document.getElementById('produit1').style.display = 'block';
-            document.getElementById('produit2').style.display = 'none';
-        });
-
-       
-        document.getElementById('produit2Link').addEventListener('click', function() {
-            document.getElementById('histoireImage').style.display = 'none';
-            document.getElementById('produit1').style.display = 'none';
-            document.getElementById('produit2').style.display = 'block';
-        });
-
 function afficherHistoire(){
     const histoire = document.getElementById('histoire');
     const button = document.getElementById('toggleButton');
@@ -24,5 +10,3 @@ function afficherHistoire(){
     }
 
 }
-
-    
--- javascript/categorie2.js
+++ javascript/categorie2.js
@@ -1,25 +1,12 @@
- document.getElementById('produit3Link').addEventListener('click', function() {
-            document.getElementById('histoireImage').style.display = 'none';
-            document.getElementById('produit3').style.display = 'block';
-            document.getElementById('produit4').style.display = 'none';
-        });
+function afficherHistoire(){
+    const histoire = document.getElementById('histoire');
+    const button = document.getElementById('toggleButton');
+    if (histoire.style.display === "none") {
+        histoire.style.display = "block";
+        button.textContent = "Cacher l'histoire";
+    } else {
+        histoire.style.display = "none";
+        button.textContent = "Afficher l'histoire";
+    }
 
-       
-        document.getElementById('produit4Link').addEventListener('click', function() {
-            document.getElementById('histoireImage').style.display = 'none';
-            document.getElementById('produit3').style.display = 'none';
-            document.getElementById('produit4').style.display = 'block';
-        });
-
-        function afficherHistoire(){
-            const histoire = document.getElementById('histoire');
-            const button = document.getElementById('toggleButton');
-            if (histoire.style.display === "none") {
-                histoire.style.display = "block";
-                button.textContent = "Cacher l'histoire";
-            } else {
-                histoire.style.display = "none";
-                button.textContent = "Afficher l'histoire";
-            }
-        
-        }
+}
